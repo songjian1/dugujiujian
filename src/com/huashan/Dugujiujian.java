d1dfa3dc35b510709ae67475236fe36842909dc5
songjian1 <1776962737@qq.com>
2017-02-28 00:35:28 +0800

    update 04

--- src/com/huashan/Dugujiujian.java
+++ src/com/huashan/Dugujiujian.java
@@ -3,5 +3,8 @@ package com.huashan;
 public class Dugujiujian {
 	public static void main(String[] args) {
 		System.out.println("hello , dugujiujian");
+		System.out.println("hello , DDDDDDDDDDDDDDDDD");
+		System.out.println("hello , DDDDDDDDDDDDDDDDDDDDD");
+		System.out.println("hello , DDDDDDDDDDDDDDDDDD");
 	}
 }
